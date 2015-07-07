cc0044ed29e5c624748126adc082538d68d97af6
beyond <beyondliangcai@163.com>
2015-07-07 15:41:00 +0800

    上传图片测试通过

--- AppointPhotoServer/WebContent/index.jsp
+++ AppointPhotoServer/WebContent/index.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>hello</title>
 </head>
 <body>
-hello
+hello jboss!
 </body>
 </html>
--- AppointPhotoServer/src/com/appointphoto/action/UserUploadImageAction.java
+++ AppointPhotoServer/src/com/appointphoto/action/UserUploadImageAction.java
@@ -6,7 +6,9 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,105 +24,116 @@ import com.opensymphony.xwork2.ModelDriven;
 //处理用户注冊
 public class UserUploadImageAction extends ActionSupport {
 
-    private static final long serialVersionUID = 1L;
-    private File photo;// 上传文件
-    private String photoFileName;// 上传文件的文件名
-    private String photoContentType;// 上传文件的类型
-    private String savePath;//上传后的保存目录
-     
-     
-    public File getPhoto() {
-        return photo;
-    }
- 
-    public void setPhoto(File photo) {
-        this.photo = photo;
-    }
- 
-    public String getPhotoFileName() {
-        return photoFileName;
-    }
- 
-    public void setPhotoFileName(String photoFileName) {
-        this.photoFileName = photoFileName;
-    }
- 
-    public String getPhotoContentType() {
-        return photoContentType;
-    }
- 
-    public void setPhotoContentType(String photoContentType) {
-        this.photoContentType = photoContentType;
-    }
- 
-    public String getSavePath() {
-        return savePath;
-    }
- 
-    public void setSavePath(String savePath) {
-        this.savePath = savePath;
-    }
- 
-    public UserUploadImageAction() {
-        System.out.println("UserUploadImageAction构造方式被执行");
-    }
-     
-    //执行上传功能
-    public String execute() {
-        System.out.println("执行上传execute（）...");
+	 // 上传文件域  
+    private File image;  
+    // 上传文件类型  
+    private String imageContentType;  
+    // 封装上传文件名  
+    private String imageFileName;  
+    // 接受依赖注入的属性  
+    private String savePath;  
+    
+    private Map<String, String> params;
+  
+    @Override  
+    public String execute() {  
         HttpServletRequest request=ServletActionContext.getRequest();  
-         
-        //1.获取上传后文件的真实目录
-        String path =ServletActionContext.getServletContext().getRealPath(this.getSavePath());
-        System.out.println(path);
-        //2.获得上传文件的真实路径
-        String f_name=path+File.separator+this.getPhotoFileName();
-        //File.separator+
-        System.out.println(f_name);
-         
-        System.out.println("文件名："+request.getParameter("fileName")); 
-        System.out.println("获取Android端传过来的文件信息：");  
-        System.out.println("文件存放目录: "+getSavePath());  
-        System.out.println("文件名称: "+photoFileName);  
-        System.out.println("文件大小: "+photo.length());  
-        System.out.println("文件类型: "+photoContentType);  
-         
-         
-        //3.创建输入和输出流
-        BufferedInputStream bis=null;
-        BufferedOutputStream bos=null;
-         
-        try {
-            bis=new BufferedInputStream(new FileInputStream(photo));
-            bos=new BufferedOutputStream(new FileOutputStream(new File(f_name)));
-            byte[] buff=new byte[(int)photo.length()];
-            int len=0;
-            while ((len=bis.read(buff))!=-1) {
-                bos.write(buff,0,len);
-            }
-             
-        } catch (Exception ex) {
-            // TODO: handle exception
-            ex.printStackTrace();
-        }
-        finally
-        {
-            try {
-                if (bis!=null) {//先close，再null
-                    bis.close();
-                    bis=null;
-                }
-                if (bos!=null) {
-                    bos.close();
-                    bos=null;
-                }
-                System.out.println("上传成功！");
-            } catch (Exception ex) {
-                // TODO: handle exception
-                ex.printStackTrace();
-                System.out.println("上传失败！");
-            }
-        }
-        return SUCCESS;
+        FileOutputStream fos = null;  
+        FileInputStream fis = null;  
+        try {  
+            System.out.println("获取Android端传过来的普通信息：");  
+            //System.out.println("用户名："+request.getParameter("username"));  
+            //System.out.println("密码："+request.getParameter("pwd"));  
+            //System.out.println("年龄："+request.getParameter("age"));  
+            System.out.println("文件名："+request.getParameter("fileName"));  
+            System.out.println("获取Android端传过来的文件信息：");  
+            System.out.println("文件存放目录: "+getSavePath());  
+            System.out.println("文件名称: "+imageFileName);  
+            System.out.println("文件大小: "+image.length());  
+            System.out.println("文件类型: "+imageContentType);  
+           // System.out.println("name: "+params.get("name").toString());  
+            
+              
+            fos = new FileOutputStream(getSavePath() + "/" + getImageFileName());  
+            fis = new FileInputStream(getImage());  
+            byte[] buffer = new byte[1024];  
+            int len = 0;  
+            while ((len = fis.read(buffer)) != -1) {  
+                fos.write(buffer, 0, len);  
+            }  
+            System.out.println("文件上传成功");  
+        } catch (Exception e) {  
+            System.out.println("文件上传失败");  
+            e.printStackTrace();  
+        } finally {  
+            close(fos, fis);  
+        }  
+        return SUCCESS;  
+    }  
+  
+    /** 
+     * 文件存放目录 
+     *  
+     * @return 
+     */  
+    public String getSavePath() throws Exception{  
+        return ServletActionContext.getServletContext().getRealPath(savePath);   
+    }  
+  
+    public void setSavePath(String savePath) {  
+        this.savePath = savePath;  
+    }  
+  
+    public File getImage() {  
+        return image;  
+    }  
+  
+    public void setImage(File image) {  
+        this.image = image;  
+    }  
+  
+    public String getImageContentType() {  
+        return imageContentType;  
+    }  
+  
+    public void setImageContentType(String imageContentType) {  
+        this.imageContentType = imageContentType;  
+    }  
+  
+    public String getImageFileName() {  
+        return imageFileName;  
+    }  
+  
+    public void setImageFileName(String imageFileName) {  
+        this.imageFileName = imageFileName;  
+    }  
+  
+    private void close(FileOutputStream fos, FileInputStream fis) {  
+        if (fis != null) {  
+            try {  
+                fis.close();  
+                fis=null;  
+            } catch (IOException e) {  
+                System.out.println("FileInputStream关闭失败");  
+                e.printStackTrace();  
+            }  
+        }  
+        if (fos != null) {  
+            try {  
+                fos.close();  
+                fis=null;  
+            } catch (IOException e) {  
+                System.out.println("FileOutputStream关闭失败");  
+                e.printStackTrace();  
+            }  
+        }  
     }
+
+	public Map<String, String> getParams() {
+		return params;
+	}
+
+	public void setParams(Map<String, String> params) {
+		this.params = params;
+	}  
 }
